8b6b2d09902f134461d749d5ea9fad01918911ec
Olga Iartseva <ol-buslaeva@ya.ru>
2022-12-24 20:48:22 +0300

    2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,21 +4,22 @@ const { UnauthorizedError } = require('../errors/allErrors');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  /* if (!req.cookies.jwt) {
+  if (!req.cookies.jwt) {
     next(new UnauthorizedError('Вы не авторизованы'));
   } else {
     const token = req.cookies.jwt;
     let payload;
 
     try {
-      payload = jwt.verify(token, JWT_SECRET);
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+      console.log(JWT_SECRET)
       req.user = payload;
       next();
     } catch (err) {
       next(new UnauthorizedError('Вы не авторизованы'));
     }
-  } */
-  const token = req.cookies.jwt;
+  }
+  /* const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
@@ -26,5 +27,5 @@ module.exports = (req, res, next) => {
     return next(new UnauthorizedError('Вы не авторизованы'));
   }
   req.user = payload;
-  return next();
+  return next(); */
 };
