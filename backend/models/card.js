ecb8ec1f92e0afbdd8810aad8239f1c4dcb212cc
Olga Iartseva <ol-buslaeva@ya.ru>
2022-12-26 14:43:13 +0300

    1review new

--- backend/models/card.js
+++ backend/models/card.js
@@ -23,16 +23,16 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  /* likes: [{
+  likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     default: [],
-  }], */
-  likes: {
+  }],
+  /* likes: {
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     default: [],
 
-  },
+  }, */
   createdAt: {
     type: Date,
     default: Date.now,
